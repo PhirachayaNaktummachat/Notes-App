98dd6bc6212b09e324ebc0b6aa05089a679d5ea3
Phirachaya Naktummachat <palmmy142@gmail.com>
2025-02-10 00:57:06 +0700

    Login, Sign Up

---
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../../components/Navbar.jsx/Navbar'
+
+const Home = () => {
+  return (
+    <>
+    <Navbar />
+    </>
+  )
+}
+
+export default Home
---
+++ frontend/notes-app/tailwind.config.ts
@@ -0,0 +1,11 @@
+export default {
+    theme: {
+      extend: {
+        colors: {
+          primary: "#2b85ff",
+          secondary: "#ef863e",
+          accent: "#FACC15",
+        }
+      }
+    }
+  };
